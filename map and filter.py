3534dd53f637b4a24a6866297e4f7450548fc4e3
Goutham <104981580+Gouthamg598@users.noreply.github.com>
2022-06-16 14:43:11 +0530

    Create map and filter.py

---
+++ map and filter.py
@@ -0,0 +1,30 @@
+# def odd(x):
+#     if x%2!=0:
+#         return True
+#     else:
+#        return False
+# print(odd(76)) #True
+'''-----taking input from list------''
+# z=[52,1,52,4,556,89]
+# r=list(filter(odd,z)) #[1, 89]
+
+# r=list(map(odd,z)) #[False, True, False, False, False, True]
+# print(r)
+'''--------------taking input values frm user in runtime-------------'''
+# b=[int(x) for x in input('enter list of int values:').split()]
+# c=list(filter(odd,b))
+# c=list(map(odd,b))
+# print(c)
+'''----------------------------------------------------------------'''
+
+# def mult(x):
+#     if x%9==0 or x%6==0:
+#         return (x)
+#     else:
+#         pass
+
+# y=[int(a) for a in input('enter the list of values').split()]
+# q=list(filter(mult,y))
+# # q=list(map(mult,y))
+# print(q)
+
